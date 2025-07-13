e5ad75cecc3eb182ced2b7ef38743f5151ed5fb8
LevionyxStudio <namankamboj04@gmail.com>
2025-07-13 11:32:13 +0530

    learning variables, datatypes

---
+++ Chapter 2/01_variable.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+c = 7
+# a, b and c is the name of the memory location or a contain which store a single value
+d = "Naman"
+print(a+b+c);
+print(d)
---
+++ Chapter 2/02_datatypes.py
@@ -0,0 +1,7 @@
+a = 1 
+b = 2
+c = 7
+# a,b,c is an integer
+d = "Naman" #d is a string
+e = False #e is a boolean variable
+f = None # f is a none variable
