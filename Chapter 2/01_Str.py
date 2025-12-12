b6fe46b078118f2235937b1121458aad184b6b84
LevionyxStudio <namankamboj04@gmail.com>
2025-12-12 10:36:06 +0530

    strings

---
+++ Chapter 2/01_Str.py
@@ -0,0 +1,6 @@
+a = 'single quote string'
+print(a)
+b = "Double quote string"
+print(b)
+c = '''triple quote string'''
+print(c)
---
+++ Chapter 2/02_Str_Slicing.py
@@ -0,0 +1,10 @@
+name = "Naman"
+# print(len(name))
+# Total Character = 5
+
+sl = name[0:2]
+# sl = name[0:3]
+print(sl)
+
+Negative_Slicing = name[-5:-3]
+print(Negative_Slicing)
