46053e9fb1449a3fd82058b1f18e22a282d8126e
LevionyxStudio <namankamboj04@gmail.com>
2025-12-19 12:45:35 +0530

    list

---
+++ Chapter 3/01_List.py
@@ -0,0 +1,10 @@
+list = ["Apple", 7, True, 3.2]
+
+# print(list)
+# print(list[0])
+
+# list are mutable
+# list[0] = "Orange"
+# list[1] = False
+# print(list)
+print(list[0:2])
---
+++ Chapter 3/02_list_methods.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = ["apple", "banana", "banana", "cherry"]
+
+# list1.append(6)
+# print(list1)
+
+# list1.clear()
+# print(list1)
+# list3 = list1.copy()
+# print(list3)
+
+# print(list2.count("banana"))
